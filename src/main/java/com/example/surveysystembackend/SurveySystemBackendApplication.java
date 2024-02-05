c99db3fa34cad3336978193dfa51db056ab3d197
krishantha-hsenidlanka <krishantha@hsenidlanka.com>
2024-02-05 15:51:02 +0530

    init dev branch

---
+++ src/main/java/com/example/surveysystembackend/SurveySystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.surveysystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveySystemBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SurveySystemBackendApplication.class, args);
+    }
+
+}
